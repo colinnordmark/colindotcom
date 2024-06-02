0d40fc4570c33757f9fa800fe2a1b21f1e10d050
colinnordmark <nordmarkcolin@gmail.com>
2024-06-02 19:39:30 +0200

    feat: configured login functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { GeistSans } from "geist/font/sans";
+import { TopNav } from "./_components/TopNav";
 
 export const metadata = {
   title: "colinnordmark",
@@ -8,23 +9,7 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const TopNav = () => {
-  return (
-    <nav className="bg-gray-800 text-white p-4">
-      <ul className="flex gap-4">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/about">Sign in</a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+
 
 export default function RootLayout({
   children,
