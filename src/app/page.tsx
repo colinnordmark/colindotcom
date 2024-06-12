c7215a940b7e6b798975f5576e6fe21950f52161
colinnordmark <nordmarkcolin@gmail.com>
2024-06-12 19:00:31 +0200

    feat: Added authorization for image access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,13 @@ import { desc } from "drizzle-orm";
 import Link from "next/link";
 import { SignedIn } from "@clerk/nextjs";
 import { db } from "~/server/db";
+import { get } from "http";
+import { getImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  "https://utfs.io/f/d9ded5b0-a5c1-47cd-b0d7-36372f53b1d3-j2rogc.jpeg",
-  "https://utfs.io/f/45bad673-70e1-4b55-ab05-891353f32908-m3fyio.jpeg",
-  "https://utfs.io/f/636f08a6-e9fe-4da7-b202-60f160e75859-f1v4e8.jpeg",
-  "https://utfs.io/f/c698a2de-b88d-4573-a76d-883923760ca2-sefrqe.jpeg"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url
-}));
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy:(model, { desc }) => desc(model.id), 
-  });
+  const images = await getImages();
 
   return (
     <div className="flex flex-wrap gap-4">
@@ -33,9 +22,6 @@ async function Images() {
   )
 }
 export default async function HomePage() {
-  
-  
-
   return (
     <main className="">
       <h1 className=" text-white">
