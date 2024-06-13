c4e6fe0d956a46f2f133b183249c46ca67665fa9
colinnordmark <nordmarkcolin@gmail.com>
2024-06-13 20:44:35 +0200

    feat: Adds analytics to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { Toaster } from "./_components/Toaster";
+import { CSPostHogProvider } from './_analytics/provider';
 
 export const metadata = {
   title: "colinnordmark",
@@ -22,22 +23,24 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
-      <html lang="en">
-        <body className={`font-sans ${GeistSans.variable}`}>
-          <TopNav/>
-          {children}
-          <Toaster />
-        </body>
-      </html>
+      <CSPostHogProvider>
+        <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+        <html lang="en">
+          <body className={`font-sans ${GeistSans.variable}`}>
+            <TopNav/>
+            {children}
+            <Toaster />
+          </body>
+        </html>
+      </CSPostHogProvider>
     </ClerkProvider>
   );
 }
