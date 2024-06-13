18eed8e2fcc7fa3fee4d398d0b93c54d191190ca
colinnordmark <nordmarkcolin@gmail.com>
2024-06-13 18:59:33 +0200

    feat: Reworks uploadbutton for cleaner layout

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -3,36 +3,24 @@
 import { SignedOut, SignedIn, SignOutButton, SignInButton, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "~/app/_components/Simple-upload-button";
 
 export const TopNav = () => {
-  const router = useRouter();
 
   return (
-      <nav className="bg-gray-800 text-white p-4">
-        <ul className="flex gap-4 text">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <div className="flex flex-row gap-4">
-                <SignOutButton />
-                <UploadButton 
-                  endpoint="imageUploader"
-                  onClientUploadComplete={() => {
-                    router.refresh();
-                  }}/>
-                <UserButton />
-              </div>
-            </SignedIn>
-          </div>
-        </ul>
-      </nav>
+    <nav className="flex w-full items-center justify-between border-b p-4">
+      <div className="flex flex-row items-center gap-4">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <SignOutButton />
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
   );
 };
